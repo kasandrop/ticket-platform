509ccf17424fe7b04b3f285d07b9a6423de466f2
kasandrop <kasandrop@gmail.com>
2024-03-15 01:09:57 +0000

    working eureka, gateway and configuration

--- gateway/src/main/java/com/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/gateway/GatewayApplication.java
@@ -1,35 +1,29 @@
 package com.gateway;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        RouteLocator routeLocator = builder.routes()
-
-                .route("seller-microservice", r -> r.path("/seller-microservice/**").uri("lb://seller-microservice")).build();
-
-
-        routeLocator.getRoutes().subscribe(route -> LOGGER.info("Read the following: RouteLocator:id: {} -> uri: {}", route.getId(), route.getUri()));
-
-        return routeLocator;
-    }
 
 }
